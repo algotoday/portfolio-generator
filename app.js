798bba26e118d110a31d97fca2d63d47772c680a
algotoday <conwaytwittyjr@gmail.com>
2022-07-20 22:53:39 -0700

    added app js js page and index.html

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
 
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-      console.log(profileDataArr[i]);
-    }
-  
-    console.log('================');
-  
-    // Is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-  };
-  printProfileData(profileDataArgs)
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   
 
 
